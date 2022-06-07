79823a9648df3ed4eed420c4d8132ad7a409517d
Jefferson Murillo <development@eticsas.co>
2022-06-07 15:29:46 -0500

    Fix: Configuración de Server ySocket.io funciona correctamente - Imple: 07-06-2022

---
+++ sockets/controller.socket.js
@@ -0,0 +1,15 @@
+const { Socket } = require("socket.io")
+
+
+const socketController = ( socket = new Socket() ) => {
+    
+    console.log( 'cliente conectado', socket.id );
+
+}
+
+
+
+
+module.exports = {
+    socketController
+}
